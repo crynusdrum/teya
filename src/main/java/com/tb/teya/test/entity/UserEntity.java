d0c1ad8ae191929ddb36b4271e95d517aff0a06b
Thiago Baldês <crynusdrum@gmail.com>
2024-09-22 22:12:13 +0100

    Entity user

---
+++ src/main/java/com/tb/teya/test/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.tb.teya.test.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class UserEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "active")
+    private boolean active;
+
+}
